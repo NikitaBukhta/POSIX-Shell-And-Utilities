3b7a473a6f93bce5de22348ebafd2647f0bab33a
NikitaBukhta <nikita.bukhta.dev@gmail.com>
2022-03-24 19:22:25 +0200

    Fixed searching task

--- bash/searching_task.sh
+++ bash/searching_task.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-# where is our script contains
+# script location
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Find the first 20 occurrences of the word "_ asm" in *.c files.
-findFirst20WordsInFiles()
+# Find the first 20 occurrences of the word "_ asm" in *.c files;
+# 
+# Output lines with occurences to the screen;
+function findFirst20WordsInFiles()
 {
+    local COUNT_OF_OCCURENCE
+    local SEARCHING_WORD
     COUNT_OF_OCCURENCE=0    # how many occurences of specific word;
-    WORD="_asm"
-    # get name of every file with .c extension;
-    for c_file in "${SCRIPT_DIR}"../src/*.c
+    SEARCHING_WORD="_asm"
+    # get name and directory of every file with .c extension;
+    # all ../c files are contained in src folder;
+    for C_FILE in "${SCRIPT_DIR}"/../src/*.c
     do
-        # get count of words of $WORD and write of in our variable;
-        COUNT_OF_OCCURENCE=$((COUNT_OF_OCCURENCE+$(grep -c $WORD "$c_file")))
-        
-    if [ "$COUNT_OF_OCCURENCE" -ge 20 ]; then break; fi
-    done
+        OUTPUT_STRING=$(grep "${SEARCHING_WORD}" "${C_FILE}")
 
-    echo "We have found ${COUNT_OF_OCCURENCE} more occurences"
+        while read -r line; do
+            echo "${line}"
+
+            # -o option is what tells grep to output each match in a unique line
+            # wc -l tells wc to count the number of lines
+            local OCCURRENCES_IN_LINE
+            OCCURRENCES_IN_LINE=$(grep -o "${SEARCHING_WORD}" <<< "${line}" | wc -l)
+            ((COUNT_OF_OCCURENCE+="${OCCURRENCES_IN_LINE}"))
+            
+            # if we found first 20 occurences, just end the function
+            if [[ ${COUNT_OF_OCCURENCE} -ge 20 ]]; then
+                break
+            fi
+        done <<< "${OUTPUT_STRING}"
+    done
 }
 
-findFirst20WordsInFiles
+function main()
+{
+    findFirst20WordsInFiles
+}
+
+main
